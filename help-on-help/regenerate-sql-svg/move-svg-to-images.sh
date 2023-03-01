25bd8e2b7440cf03c17fd20e8e110ec13fd69b26
LJ Sinclair <48849797+ljsinclair@users.noreply.github.com>
2023-03-01 19:39:44 +1100

    updated script that works on commandline

--- help-on-help/regenerate-sql-svg/move-svg-to-images.sh
+++ help-on-help/regenerate-sql-svg/move-svg-to-images.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
-
-# Command to execute
-execute_cmd=mv
-
-path="~/featurebase-docs"
-
-# Files inside actual_dir has to be moved
-actual_dir="$path/help-on-help"
-
-# This is the directory where files will be moved and kept
-backup_dir="$path/assets/images/sql-guide"
-
-# Move each file from actual_dir to backup_dir
-
-echo "Moving files to backup_dir"
-
-for f in $(find $actual_dir -type f -name *.xlsx); ## used find here, with semicolon
-do
- echo $f
- $execute_cmd $f $backup_dir
-done
-
-echo "Moving of files completed"
+echo moving all .svg files from /help-on-help to /assets/images/sql-guide
+cd ..
+mv *.svg ~/featurebase-docs/assets/images/sql-guide
