d5c2a702da2f4790d37b6b894cfe4d1f59f7d174
LJ Sinclair <48849797+ljsinclair@users.noreply.github.com>
2023-03-01 20:28:35 +1100

    minor addition

--- help-on-help/regenerate-sql-svg/move-svg-to-images.sh
+++ help-on-help/regenerate-sql-svg/move-svg-to-images.sh
@@ -2,3 +2,4 @@
 echo moving all .svg files from /help-on-help to /assets/images/sql-guide
 cd ..
 mv *.svg ~/featurebase-docs/assets/images/sql-guide
+echo Move complete. See results in /assets/images/sql-guide
