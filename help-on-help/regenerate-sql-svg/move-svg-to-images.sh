1f48e484580aeab1f4f12b0a705317c44627214d
LJ Sinclair <48849797+ljsinclair@users.noreply.github.com>
2023-03-01 19:28:27 +1100

    rejigged the move script from das interwebs

--- help-on-help/regenerate-sql-svg/move-svg-to-images.sh
+++ help-on-help/regenerate-sql-svg/move-svg-to-images.sh
@@ -1,4 +1,24 @@
-echo Moving svg files to /assets/images/sql-guide...
+#!/bin/bash
 
-cd ..
-mv *.svg /assets/images/sql-guide
+# Command to execute
+execute_cmd=mv
+
+path="/home/featurebase-docs"
+
+# Files inside actual_dir has to be moved
+actual_dir="$path/help-on-help"
+
+# This is the directory where files will be moved and kept
+backup_dir="$path/assets/images/sql-guide"
+
+# Move each file from actual_dir to backup_dir
+
+echo "Moving files to backup_dir"
+
+for f in $(find $actual_dir -type f -name *.xlsx); ## used find here, with semicolon
+do
+ echo $f
+ $execute_cmd $f $backup_dir
+done
+
+echo "Moving of files completed"
